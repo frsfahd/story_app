bb086f29ffd8de0fc8739bf5bff96deca1bd21af
frsfahd <farraselfahd2000@gmail.com>
2026-01-17 09:18:15 +0700

    clean : unused imports and commented code

--- lib/models/generic_response.dart
+++ lib/models/generic_response.dart
@@ -1,24 +1,3 @@
-// response object for various api requests (register, add new story, add new story as guest)
-
-import 'dart:convert';
-
-// class GenericResponse {
-//   bool error;
-//   String message;
-
-//   GenericResponse({required this.error, required this.message});
-
-//   factory GenericResponse.fromRawJson(String str) =>
-//       GenericResponse.fromJson(json.decode(str));
-
-//   String toRawJson() => json.encode(toJson());
-
-//   factory GenericResponse.fromJson(Map<String, dynamic> json) =>
-//       GenericResponse(error: json["error"], message: json["message"]);
-
-//   Map<String, dynamic> toJson() => {"error": error, "message": message};
-// }
-
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'generic_response.g.dart';
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,31 +1,4 @@
-import 'dart:convert';
-
-import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
-
-// class User extends Equatable {
-//   final String userId;
-//   final String name;
-//   final String email;
-
-//   const User({required this.userId, required this.name, required this.email});
-
-//   factory User.fromRawJson(String str) => User.fromJson(json.decode(str));
-
-//   String toRawJson() => json.encode(toJson());
-
-//   factory User.fromJson(Map<String, dynamic> json) =>
-//       User(userId: json["userId"], name: json["name"], email: json["email"]);
-
-//   Map<String, dynamic> toJson() => {
-//     "userId": userId,
-//     "name": name,
-//     "email": email,
-//   };
-
-//   @override
-//   List<Object?> get props => [userId, name, email];
-// }
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 
